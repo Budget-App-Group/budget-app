9e3705550b9252cf50ae73ba5268ff8a83598609
Colby B <colby.birrell@gmail.com>
2020-04-03 12:54:41 -0600

    Initial push, no packages yet

---
+++ server/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+// const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+
+// app.listen(SERVER_PORT, () =>
+//   console.log(`<--- SERVER RUNNING ON PORT ${SERVER_PORT} --->`)
+// );
