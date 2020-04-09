9ba55b37761cf6f75f2a44588890499698ac314b
user <user@users-MacBook-Air.local>
2020-04-09 17:00:34 -0600

    whit

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -7,26 +7,15 @@ import Messages from "../Message/Messages/Messages";
 import InfoBar from "../InfoBar/InfoBar";
 import Input from "../input/Input";
 
-<<<<<<< HEAD
-import './Chat.css'
-=======
 import "./Chat.css";
->>>>>>> e15e703867ae5884a2f6f0a53ffa00e414c50a8a
 
 let socket;
 
 const Chat = ({ location }) => {
-<<<<<<< HEAD
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [users, setUsers] = useState('');
-  const [message, setMessage] = useState('');
-=======
   const [username, setName] = useState("");
   const [room, setRoom] = useState("");
   const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
->>>>>>> e15e703867ae5884a2f6f0a53ffa00e414c50a8a
   const [messages, setMessages] = useState([]);
   const ENDPOINT = "https://localhost:4242";
 
@@ -36,17 +25,10 @@ const Chat = ({ location }) => {
     socket = io(ENDPOINT);
 
     setRoom(room);
-<<<<<<< HEAD
-    setName(name)
-
-    socket.emit('join', { name, room }, (error) => {
-      if(error) {
-=======
     setName(username);
 
     socket.emit("join", { username, room }, (error) => {
       if (error) {
->>>>>>> e15e703867ae5884a2f6f0a53ffa00e414c50a8a
         alert(error);
       }
     });
@@ -73,11 +55,6 @@ const Chat = ({ location }) => {
   return (
     <div className="outerContainer">
       <div className="container">
-<<<<<<< HEAD
-          <InfoBar room={room} />
-          <Messages messages={messages} name={name} />
-          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-=======
         <InfoBar room={room} />
         <Messages messages={messages} username={username} />
         <Input
@@ -85,7 +62,6 @@ const Chat = ({ location }) => {
           setMessage={setMessage}
           sendMessage={sendMessage}
         />
->>>>>>> e15e703867ae5884a2f6f0a53ffa00e414c50a8a
       </div>
       <TextContainer users={users} />
     </div>
